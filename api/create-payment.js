e482b3593c78e6eecb9ba087831d9978e4eef842
stepoil-debug <stepoil388@gmail.com>
2026-09-11 19:00:39 -0300

    Configurar InfiniteTag correta no checkout

--- api/create-payment.js
+++ api/create-payment.js
@@ -2,7 +2,7 @@ const { randomBytes } = require('node:crypto');
 
 const PRICE_CENTS = 4990;
 const SITE_URL = 'https://cancao.dflabs.app';
-const FALLBACK_HANDLE = 'lessence';
+const FALLBACK_HANDLE = 'fran-doug-65a';
 
 function getHandle() {
   return (process.env.INFINITEPAY_HANDLE || FALLBACK_HANDLE).replace(/^\$/, '').trim();
